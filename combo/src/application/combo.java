7c106f3f45dad1e0868bb3a81bbb60cb0b28bb76
sirishavarma <sirishavarma@192.168.2.13>
2015-05-10 22:32:56 -0400

    com

---
+++ combo/src/application/combo.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ComboBoxBase;
+import javafx.stage.Stage;
+
+public class combo {
+	 @SuppressWarnings("rawtypes")
+	@FXML
+	    final ComboBox monthBox = new ComboBox();
+	    @SuppressWarnings("rawtypes")
+		@FXML
+	    final ComboBox dayBox = new ComboBox(); 
+	    @SuppressWarnings("rawtypes")
+		@FXML
+	    final ComboBox yearBox = new ComboBox();
+	    @FXML
+	    public void month(){jan, feb, mar, apr, may,jun,jul,aug,spe,oct,nov,dec}
+	    @FXML
+	    public void day(){1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31}
+	    @FXML
+	    public void year{05,06,07,08,09,10,11,12,13,14,15}
